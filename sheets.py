18cb54c41ed9132c02aa768868df51b4ca5ecbcc
Andrea Acevedo <andreaacevedo@192.168.2.109>
2026-07-13 18:16:37 -0500

    feat(fase-2): conexión a Google Sheets y función registrar_gasto

---
+++ sheets.py
@@ -0,0 +1,30 @@
+import gspread
+from google.oauth2.service_account import Credentials
+import json
+import os
+import base64
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_sheet(): 
+    raw = os.getenv("GOOGLE_CREDENTIALS_B64")
+    sheet_id = os.getenv("GOOGLE_SHEETS_ID")
+
+    decoded_credentials = base64.b64decode(raw).decode('utf-8')
+    credentials_dict = json.loads(decoded_credentials)
+    creds = Credentials.from_service_account_info(
+        credentials_dict,
+        scopes=["https://www.googleapis.com/auth/spreadsheets"])
+    client = gspread.authorize(creds) 
+    sheet = client.open_by_key(sheet_id)
+    return sheet.worksheet("gastos")
+
+def registrar_gasto(fecha, monto, categoria, medio):
+    sheet = get_sheet()
+    sheet.append_row([fecha, monto, categoria, medio])
+
+
+
+
+
